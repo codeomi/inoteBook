f093a1b4fcd8b70247fabfeb55d204f001d2348f
Omkar Kudale <omkarkudale007@gmail.com>
2022-12-02 16:43:39 +0530

    Storing data into database using mongoose model(45).

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const mongoUri = "mongodb://localhost:27017/"
+
+
+const connectToMongo= ()=>{
+    mongoose.connect(mongoUri,()=>{
+        console.log("Connected to Mongo succedfully");
+    })
+}
+
+module.exports=connectToMongo
---
+++ backend/routes/auth.js
@@ -0,0 +1,15 @@
+//AUTHENTICATION RELATED END-POINTS WILL GO HERE
+const express = require("express")//importing express
+const User= require('../models/User')//importing user
+const router = express.Router()
+
+
+//Create a User using: POST "/api/auth/". Doesnt require auth(authentication)
+router.post('/',(req,res)=>{
+    console.log(req.body)
+    const user= User(req.body)
+    user.save()
+    res.send(req.body)
+})
+
+module.exports=router
