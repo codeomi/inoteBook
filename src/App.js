9df9ec588fb6c74a28a551ae0bec4f19c8b8f9c1
Omkar Kudale <omkarkudale007@gmail.com>
2022-12-17 14:51:47 +0530

    Created Navbar and basic Routes(56).

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNoteBook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route exact path="/home" element={ <Home />}>
+          </Route>
+          <Route exact path="/about" element={<About />}>
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
